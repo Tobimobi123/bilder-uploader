59a716ddcb9948d4d080efe80ef9441b83ec240a
Tobimobi123 <tobiasmatern01@gmail.com>
2025-04-30 13:22:25 +0200

    Bildoptimierung für BMP deaktiviert

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      unoptimized: true, // Deaktiviert die automatische Optimierung
+    },
+  };
+  
+  module.exports = nextConfig;
+  
